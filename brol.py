306981620c77726fb5dcfe2e24d7f148c4d298ff
Lander Moerkerke <lander.moerkerke@telenet.be>
2019-02-26 13:06:19 +0100

    git quick push

--- brol.py
+++ brol.py
@@ -1,16 +1,3 @@
-import datetime
-from model.Client import Client
-from django.forms.models import model_to_dict
-from bson import json_util
-import json
-
-# data = '{"dateLogin":1524594373,"dateLogout":null,"email":"mlkj","messages":[],"name":"qdsfqsf","nickname":"lkqsdjmf","online":true}'
-# dc = json.loads(data, object_hook=json_util.object_hook)
-# print(dc)
-# # print(datetime.datetime(dc["dateLogin"])
-
-# cl = Client("Test", "jan", "jlkq")
-# test = json.dumps(cl.__dict__, default=json_util.default)
 # tlksdj = json.loads(data, object_hook=json_util.object_hook)
 # print(tlksdj)
 
